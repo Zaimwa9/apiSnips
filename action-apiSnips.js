b71008fb56bedbd81c696e0b7dfe12895107037f
wadii <wadii.zaim@essec.edu>
2018-10-08 10:01:35 +0000

    finished adding product process + cancel

--- action-apiSnips.js
+++ action-apiSnips.js
@@ -53,39 +53,6 @@ client.on('message', function(topic, message) {
 
 	if (topic == 'hermes/intent/wzaim:getWebsite') {
 		initDb(db, payload);
-		/*	if (!fs.existsSync(db)) {
-			console.log('No file, creating...');
-			var content = {
-				"website": {
-					"sockets": [
-						{
-							"name": "temperature",
-							"active": false,
-						},
-						{
-							"name": "humidity",
-							"active": true
-						},
-						{
-							"name": "pressure",
-							"active": true
-						}
-					]
-				}
-			}
-			try {
-				fs.writeFileSync(db, content);
-			} catch (e) {
-				throw (e);
-				console.log(e);
-				var resp = {
-					"sessionId": payload.sessionId,
-					"text": "Erreur lors de l'initialisation de la base de donnees"
-				}
-				client.publish('hermes/dialogueManager/endSession', JSON.stringify(resp));
-			}
-		}
-	*/
 		var data = fs.readFileSync(db);
 		data = JSON.parse(data);
 		if (data.length === 0 || data.website.length === 0) {
@@ -123,8 +90,10 @@ client.on('message', function(topic, message) {
 		var data = fs.readFileSync(db);
 		data = JSON.parse(data);
 		if (!('products' in data.website)) {
+			newProduct['id'] = 1;
 			var products = [newProduct];
 		} else {
+			newProduct['id'] = Math.floor(products.length * (Math.random * 5));
 			var products = data.website.products;
 			products.push(newProduct);
 		}
@@ -194,5 +163,47 @@ client.on('message', function(topic, message) {
 			}
 			client.publish('hermes/dialogueManager/endSession', JSON.stringify(resp));
 		}
+	}
+
+	if (topic == 'hermes/intent/wzaim:addStock') {
+		initDb(db, payload);
+		var data = fs.readFileSync(db);
+		data = JSON.parse(data);
+		var products = data.website.products;
+		var newStock = payload.slots.length > 0 ? payload.slots[0].value.value : "";
+
+		if (products[product.length - 1].name.length > 0 && products[products.length - 1].price.length > 0 && products[products.length - 1].stock.length == 0) {
+			if (payload.slots.length > 0) {
+				products[products.length - 1].stock = newStock;
+			}
+			products = products.filter(product => product.name.length > 0 && product.price > 0 && product.stock >= 0);
+			console.log(products);
+			data = {...data, website: {...data.website, products: products}};
+			fs.writeFileSync(db, JSON.stringify(data));
+			var resp = {
+				'sessionId': payload.sessionId,
+				'text': `Le produit ${products[products.length - 1].name} a ete ajoute avec succes au prix de ${products[products.length - 1].price} avec un stock de ${newStock}.`
+			}
+		} else {
+			var resp = {
+				'sessionId': payload.sessionId,
+				'text': "Aucun produit en cours de creation, merci d'en creer un avant de vouloir ajouter son stock"
+			}
+		}
+	}
+
+	if (topic == 'hermes/intent/wzaim:cancel') {
+		initDb(db, payload);
+		var data = fs.readFileSync(db);
+		data = JSON.parse(data);
+		var products = data.website.products;
+		products = products.filter(product => product.name.length > 0 && product.price > 0 && product.stock >= 0);
+		data = {...data, website: {...data.website, products: products}};
+		fs.writeFileSync(db, JSON.stringify(data));
+		var resp = {
+			'sessionId': payload.sessionId,
+			'text': `Creation de produit annulee et base de donnees nettoyee.`
+		}
+		client.publish('hermes/dialogueManager/endSession', JSON.stringify(resp));
 	}
 })
